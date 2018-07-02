fe2fa3772a8e7db2c5a229a3e7045ab2c8ebe1fd
chendaxian <1140701884@qq.com>
2018-07-03 00:36:46 +0800

    fixed upload img

--- resources/views/admin/commodity/index.blade.php
+++ resources/views/admin/commodity/index.blade.php
@@ -141,7 +141,7 @@
                                                 </td>
                                                 <td>{{$v->created_at}}</td>
                                                 <td>
-                                                    <button class="btn btn-info" onclick="editCommodity()">编辑</button>
+                                                    <button class="btn btn-info" onclick="editCommodity('{{$v->id}}', '{{$v->name}}', '{{$v->price}}', '{{$v->express_price}}', '{{$v->sale_amounts}}', '{{$v->location}}', '{{$v->is_shelves}}', '{{$v->img}}')">编辑</button>
                                                     <a href="{{ route('commodityDelete', ['id' => $v->id]) }}">
                                                         <button class="btn btn-danger" onclick="">删除</button>
                                                     </a>
@@ -174,7 +174,7 @@
     </div>
 
     {{-- 编辑模态框 --}}
-    <div class="modal fade" id="editModal" tabindex="-1" role="dialog">
+    <div class="modal fade" id="editModal" commdityId="" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -183,63 +183,73 @@
                 </div>
                 <div class="modal-body">
                     <div class="row">
-                        <div class="col-md-2 text-right">
-                            <span>商品标题：</span>
+                        <div class="col-md-3 text-right">
+                            <span>商品名称:</span>
                         </div>
-                        <div class="col-md-10">
-                            <textarea class="form-control" style="min-height:70px !important;"></textarea>
-                        </div>
-                    </div>
-                    <div class="row m-t-5">
-                        <div class="col-md-2 text-right">
-                            <span>商品售价：</span>
-                        </div>
-                        <div class="col-md-10">
-                            <input type="text" name="original_price" class="form-control">
+                        <div class="col-md-9">
+                            <input class="form-control" id="name" name="name">
                         </div>
                     </div>
                     <div class="row m-t-5">
-                        <div class="col-md-2 text-right">
-                            <span>物流费用：</span>
+                        <div class="col-md-3 text-right">
+                            <span>商品售价(￥):</span>
                         </div>
-                        <div class="col-md-10">
-                            <input type="text" name="express_price" class="form-control">
-                        </div>
-                    </div>
-                    <div class="row m-t-5">
-                        <div class="col-md-2 text-right">
-                            <span>商品销量：</span>
-                        </div>
-                        <div class="col-md-10">
-                            <input type="text" name="sale_amounts" class="form-control">
+                        <div class="col-md-9">
+                            <input type="text" id="price" name="price" class="form-control">
                         </div>
                     </div>
                     <div class="row m-t-5">
-                        <div class="col-md-2 text-right">
-                            <span>始发地：</span>
+                        <div class="col-md-3 text-right">
+                            <span>物流费用(￥):</span>
                         </div>
-                        <div class="col-md-10">
-                            <input type="text" name="location" class="form-control">
+                        <div class="col-md-9">
+                            <input type="text" id="express_price" name="express_price" class="form-control">
                         </div>
                     </div>
                     <div class="row m-t-5">
-                        <div class="col-md-2 text-right">
-                            <span>上架情况：</span>
+                        <div class="col-md-3 text-right">
+                            <span>商品销量:</span>
                         </div>
-                        <div class="col-md-10">
-                            <select class="form-control" name="is_shelves">
+                        <div class="col-md-9">
+                            <input type="text" id="sale_amounts" name="sale_amounts" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row m-t-5">
+                        <div class="col-md-3 text-right">
+                            <span>始发地:</span>
+                        </div>
+                        <div class="col-md-9">
+                            <input type="text" id="location" name="location" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row m-t-5">
+                        <div class="col-md-3 text-right">
+                            <span>上架情况:</span>
+                        </div>
+                        <div class="col-md-9">
+                            <select class="form-control" id="is_shelves" name="is_shelves">
                                 <option value="">请选择上架情况</option>
                                 <option value="0">上架</option>
                                 <option value="1">下架</option>
                             </select>
                         </div>
                     </div>
+
                     <div class="row m-t-5">
-                        <div class="col-md-2 text-right">
-                            <span>商品预览：</span>
+                        <div class="col-md-3 text-right">
+                            <span>商品图片:</span>
                         </div>
-                        <div class="col-md-10">
-                            <img width="200px" height="200px" src="{{URL::asset('img/test.jpg')}}">
+                        <div class="col-md-9">
+                            <input type="file" style="display: none;" id="uploadImg" name="img" onchange="contentChange()">
+                            <button class="btn btn-info" onclick="uploadFile()">点击上传</button>
+                        </div>
+                    </div>
+
+                    <div class="row m-t-5">
+                        <div class="col-md-3 text-right">
+                            <span>商品预览:</span>
+                        </div>
+                        <div class="col-md-9" id="priviewImg">
                         </div>
                     </div>
                 </div>
@@ -253,8 +263,85 @@
 @endsection
 @section('bottom_script')
 <script type="text/javascript">
-    function editCommodity() {
+    // 展开modal
+    function editCommodity(id, name, price, express_price, sale_amounts, location, is_shelves, img) {
+        $('#editModal').attr('commdityId', id);
+        var array = ['name', 'price', 'express_price', 'sale_amounts', 'location', 'is_shelves'];
+        var defaultImg = "{{URL::asset('img/nothing.png')}}";
+        for(var i = 0; i < array.length; i++) {
+            $('#'+array[i]).val(eval(array[i]));
+        }
+        if (!img) {
+            var image = "<img width='150px' height='112px' src='"+defaultImg+"'>";
+        } else {
+            var image = new Image();
+            image.src = img;
+            image.hidden = 'hidden';
+
+            image.onload = function () {
+                var width = image.width, height = image.height;
+                image.width = 150;
+                image.height = 150*(height/width);
+                image.hidden = null;
+            };
+        }
+        $('#priviewImg').html('');
+        $('#priviewImg').append(image);
         $('#editModal').modal('show');
+    }
+    // 上传
+    function uploadFile() {
+        $('#uploadImg').click();
+    }
+
+    // 渲染
+    function contentChange() {
+        var file = $('#uploadImg').prop('files');
+        if (file[0].type.indexOf('image') == -1) {
+            alert('您上传的文件不是图片，请核对后再试！');
+        }
+
+        if (typeof (FileReader) === 'undefined') {
+            alert('您的浏览器不支持FileReader，请使用最新的chrome');
+            return false;
+        }
+        if(!/image/.test(file[0].type)){
+            return false;
+        };
+        var reader = new FileReader();
+        reader.readAsDataURL(file[0]);
+        reader.onload = function(){
+            var data = this.result;
+            var image = new Image();
+            image.src = data;
+            image.hidden = 'hidden';
+
+            image.onload = function () {
+                var width = image.width, height = image.height;
+                image.width = 150;
+                image.height = 150*(height/width);
+                image.hidden = null;
+            };
+
+            $('#priviewImg').html('');
+            $('#priviewImg').append(image);
+        }
+    }
+
+    // 图片渲染公共函数
+    function imgMake(src, priviewDiv) {
+        var image = new Image();
+        image.src = src;
+        image.hidden = 'hidden';
+        $('#'+priviewDiv).html('');
+        $('#'+priviewDiv).append(image);
+
+        image.onload = function () {
+            var width = image.width, height = image.height;
+            image.width = 150;
+            image.height = 150*(height/width);
+            image.hidden = null;
+        };
     }
 </script>
 @endsection
